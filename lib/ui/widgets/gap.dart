dc96aed5cfc0024885d687b498868472d22104bf
Danillo Ilggner <danilloilggner@gmail.com>
2022-07-18 12:22:07 -0300

    refactor: remove unused gaps

--- lib/ui/widgets/gap.dart
+++ lib/ui/widgets/gap.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
+const gapH20 = SizedBox(height: 20);
 const gapH30 = SizedBox(height: 30);
 const gapH45 = SizedBox(height: 45);
 const gapH50 = SizedBox(height: 50);
-const gapH70 = SizedBox(height: 70);
